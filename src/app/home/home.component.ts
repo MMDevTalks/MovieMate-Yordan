cc0df53e7f7699940ff7c69f557757e888325434
Yordan Georgiev <yordan.georgiev@icb.bg>
2018-02-09 16:58:51 +0200

    Homework 3 - unfinished part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../core/services/movie.service';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mm-home',
@@ -12,9 +13,12 @@ export class HomeComponent implements OnInit {
   public movies$: Observable<Array<any>>;
   public selectedMovie: any;
 
-  constructor (private _movieService: MovieService) {
+  constructor (private _movieService: MovieService, private _router: Router) {
   }
 
+  shownMovieDetails(movie) {
+    this._router.navigate([{outlets: {'sidebar': `details/${movie.id}`}}]);
+  }
   selectMovie(movie) {
     this.selectedMovie = movie;
   }
--- src/app/shared/components/movie-poster/movie-poster.component.ts
+++ src/app/shared/components/movie-poster/movie-poster.component.ts
@@ -9,6 +9,7 @@ export class MoviePosterComponent implements OnInit {
 
 @Input() movie: any = {};
 @Output() selectMovie: EventEmitter<any> = new EventEmitter();
+@Output() movieDetailsShown: EventEmitter<any> = new EventEmitter();
 @HostBinding('class.movie-poster--active') @Input() isSelected = false;
 
   constructor() { }
@@ -16,6 +17,9 @@ export class MoviePosterComponent implements OnInit {
   ngOnInit() {
   }
 
+  showMovieDetails (movie: any) {
+    this.movieDetailsShown.emit(movie);
+  }
   clickMoviePoster (event: Event, movie, doNotPropagate?: boolean) {
     if (doNotPropagate) {
       event.stopPropagation();
